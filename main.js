33f99e6b0f82f182c48054c94a4f44d71dbda522
Bountiful Square <bountifulsquare4@gmail.com>
2021-07-11 09:48:00 +0800

    refactor renderer & scene into world

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-import root from './src/_root.js'
+import World from './src/_world.js'
 import sky from './src/_sky.js'
-import { CameraSwitch_dev } from './src/_camera.js'
+import { CameraSwitch_dev, FirstPersonCamera, OrbitCamera } from './src/_camera.js'
 import Transform_dev from './src/_transform.js'
+import { Box, Sphere } from './src/_primitives.js'
 
-(function main() {
+window.ENV = 'dev'
+
+;(function main() {
     const canvas = document.getElementById('canvas')
-    const { renderer, scene, stats } = root(canvas)
+    const world = new World(canvas)
     const camera = new CameraSwitch_dev(canvas)
-    const transform = new Transform_dev(camera, canvas)
 
-    scene.add(sky)
+    world.addChildren(sky)
 
     function run() {
-        renderer.render(scene, camera.Object3D)
-
-        stats.update()
+        world.render(camera.Object3D)
         requestAnimationFrame(run)
     }
     run()
 
     window.addEventListener('resize', () => {
         camera.updateAspect(window.innerWidth/window.innerHeight)
-        renderer.setSize(window.innerWidth, window.innerHeight)
+        world.setSize(window.innerWidth, window.innerHeight)
     })
 })()
